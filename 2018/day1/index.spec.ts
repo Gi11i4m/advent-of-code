bc30befa9bd42e2351de68ffdb76cb47d373808e
Gi11i4m <gi11i4m@gmail.com>
2019-02-05 23:46:57 +0100

    Update

--- 2018/day1/index.spec.ts
+++ 2018/day1/index.spec.ts
@@ -1,5 +1,8 @@
+import 'mocha';
+import assert = require('assert');
+
 describe('Frequency calculation', () => {
   it('should calculate frequency drift', () => {
-    
+    assert(1).toBe(1);
   });
 });
--- 2018/day1/index.ts
+++ 2018/day1/index.ts
@@ -1,5 +1,6 @@
 export function calculateFrequencyDrift(frequencyChanges: Array<number>) {
-    return frequencyChanges.reduce((freq1, freq2) => freq1 + freq2, 0);
+  console.log('Working');
+  return frequencyChanges.reduce((freq1, freq2) => freq1 + freq2, 0);
 }
 
 calculateFrequencyDrift([]);
